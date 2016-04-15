99ce09d391cd0128e5f13b8766654d5f1e7bff00
mfzhang412 <MFZhang@student.naperville203.org>
2016-04-15 10:31:03 -0500

    Day 2

--- Gravity/ControlPanel.java
+++ Gravity/ControlPanel.java
@@ -15,34 +15,40 @@ public class ControlPanel
 {
     /** description of instance variable x (add comment for each instance variable) */
     private JButton set;
-    private JOptionPane
+    private JOptionPane mass;
+    private JOptionPane volume;
     
     private DrawingPanel panel;
-
+    private System s;
+    
+    private double mass;
+    private double volume;
+    
     /**
      * Default constructor for objects of class ControlPanel
      */
-    public ControlPanel()
+    public ControlPanel(DrawingPanel panel)
     {
-        // initialise instance variables
-        x = 0;
+        this.panel = panel;
+        
+        this.set = new JButton("Set Parameters");
+        
+        this.mass = new JOptionPane("Mass of object");
+        //mass = joption pane to get mass
+    
+        this.volume = new JOptionPane("Volume of object");
+        //volume = joption pane to get volume
+        
+        set.addActionListener(new SetListener());
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *    that describes the operation of the method
-     *
-     * @pre        preconditions for the method
-     *            (what the method assumes about the method's parameters and class's state)
-     * @post    postconditions for the method
-     *            (what the method guarantees upon completion)
-     * @param    y    description of parameter y
-     * @return    description of the return value
-     */
-    public int sampleMethod(int y)
+    public class SetListener implements ActionListener
     {
-        // put your code here
-        return x+y;
+        public void actionPerformed(ActionEvent e)
+        {
+            s.setMass(mass);
+            s.setVolume(volume);
+        }
     }
 
 }
